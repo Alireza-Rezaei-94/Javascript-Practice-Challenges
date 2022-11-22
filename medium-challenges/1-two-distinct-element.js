b607dcb63f41ca17b8d7b249807b06b0f5760590
AlirezaRezaei94 <rezaeialireza1994@gmail.com>
2022-11-23 02:26:41 +0330

    javaScript-Practice-3A-AlirezaRezaei
    javaScript-Practice-3A-AlirezaRezaei

--- medium-challenges/1-two-distinct-element.js
+++ medium-challenges/1-two-distinct-element.js
@@ -1,11 +1,47 @@
 // In each input array, every number repeats at least once,
 //  except for two. Write a function that returns the two unique numbers.
 
+function returnUnique(arr) {
+  let finalList = [];
+  let isRepeated = [];
+  arr.forEach((element) => {
+    if (finalList.includes(element) === false) {
+      finalList.push(element);
+    } else {
+      if (isRepeated.includes(element) === false) {
+        isRepeated.push(element);
+      }
+    }
+    // console.log(finalList);
+    // console.log(isRepeated);
+  });
+  let copyOffinlalList = [];
+  finalList.forEach((element) => {
+    // console.log(isRepeated.includes(element), "NowElement", element);
+    // console.log("finalInForeach", finalList);
+    if (isRepeated.includes(element)) {
+      //   console.log("indexOF", finalList.indexOf(element));
+      finalList.splice(finalList.indexOf(element), 1, 0);
+    }
+    // console.log("FirstFinal", finalList);
+  });
+  finalList.forEach((element) => {
+    if (element !== 0) {
+      copyOffinlalList.push(element);
+    }
+  });
+  console.log("------------");
+  //   console.log("finalList", finalList);
+  console.log("copyOffinlalList", copyOffinlalList);
+  console.log("isRepeated", isRepeated);
+}
+
 // Examples
-// returnUnique([1, 9, 8, 8, 7, 6, 1, 6]) ➞ [9, 7]
+// returnUnique([1, 9, 8, 8, 7, 6, 1, 6, 6])➞ [9, 7]
 
-// returnUnique([5, 5, 2, 4, 4, 4, 9, 9, 9, 1]) ➞ [2, 1]
+// returnUnique([5, 5, 2, 4, 4, 4, 9, 9, 9, 1])➞ [2, 1]
 
-// returnUnique([9, 5, 6, 8, 7, 7, 1, 1, 1, 1, 1, 9, 8]) ➞ [5, 6]
+// returnUnique([9, 5, 6, 8, 7, 7, 1, 1, 1, 1, 1, 9, 8]) 
+// ➞ [5, 6]
 // Notes
 // Keep the same ordering in the output.
--- medium-challenges/2-minesWeeper.js
+++ medium-challenges/2-minesWeeper.js
@@ -1,9 +1,147 @@
 // This challenge is based on the game Minesweeper.
 
-// Create a function that takes a grid of # and -, where each hash (#) 
-// represents a mine and each dash (-) represents a mine-free spot. 
+// Create a function that takes a grid of # and -, where each hash (#)
+// represents a mine and each dash (-) represents a mine-free spot.
 // Return an array where each dash is replaced by a digit indicating the number
 //  of mines immediately adjacent to the spot (horizontally, vertically, and diagonally).
+
+function numGrid(array) {
+  var myGrid = [...Array(5)].map((e) => Array(5));
+  var myGrid = [...Array(6)].map((e) => Array(6).fill(0));
+  for (let i = 0; i < 5; i++) {
+    // console.log("i", i);
+    for (let j = 0; j < 5; j++) {
+      //   console.log(array[i][j]);
+      //   console.log("j", j);
+      //   console.log("index", array.indexOf(array[i][j]));
+      if (array[i][j] === "#") {
+        if (
+          i - 1 >= 0 &&
+          j - 1 >= 0 &&
+          i - 1 <= 4 &&
+          j - 1 <= 4 &&
+          array[i - 1][j - 1] !== "#"
+        ) {
+          if (array[i - 1][j - 1] !== "-") {
+            array[i - 1][j - 1] = String(Number(array[i - 1][j - 1]) + 1);
+          } else {
+            array[i - 1][j - 1] = "1";
+          }
+        }
+        if (
+          i - 1 >= 0 &&
+          j >= 0 &&
+          i - 1 <= 4 &&
+          j <= 4 &&
+          array[i - 1][j] !== "#"
+        ) {
+          if (array[i - 1][j] !== "-") {
+            array[i - 1][j] = String(Number(array[i - 1][j]) + 1);
+          } else {
+            array[i - 1][j] = "1";
+          }
+        }
+        if (
+          i - 1 >= 0 &&
+          j + 1 >= 0 &&
+          i - 1 <= 4 &&
+          j + 1 <= 4 &&
+          array[i - 1][j + 1] !== "#"
+        ) {
+          if (array[i - 1][j + 1] !== "-") {
+            array[i - 1][j + 1] = String(Number(array[i - 1][j + 1]) + 1);
+          } else {
+            array[i - 1][j + 1] = "1";
+          }
+        }
+
+        if (
+          i >= 0 &&
+          j - 1 >= 0 &&
+          i <= 4 &&
+          j - 1 <= 4 &&
+          array[i][j - 1] !== "#"
+        ) {
+          if (array[i][j - 1] !== "-") {
+            array[i][j - 1] = String(Number(array[i][j - 1]) + 1);
+          } else {
+            array[i][j - 1] = "1";
+          }
+        }
+
+        if (
+          i >= 0 &&
+          j + 1 >= 0 &&
+          i <= 4 &&
+          j + 1 <= 4 &&
+          array[i][j + 1] !== "#"
+        ) {
+          if (array[i][j + 1] !== "-") {
+            array[i][j + 1] = String(Number(array[i][j + 1]) + 1);
+          } else {
+            array[i][j + 1] = "1";
+          }
+        }
+
+        if (
+          i + 1 >= 0 &&
+          j - 1 >= 0 &&
+          i + 1 <= 4 &&
+          j - 1 <= 4 &&
+          array[i + 1][j - 1] !== "#"
+        ) {
+          if (array[i + 1][j - 1] !== "-") {
+            array[i + 1][j - 1] = String(Number(array[i + 1][j - 1]) + 1);
+          } else {
+            array[i + 1][j - 1] = "1";
+          }
+        }
+        if (
+          i + 1 >= 0 &&
+          j >= 0 &&
+          i + 1 <= 4 &&
+          j <= 4 &&
+          array[i + 1][j] !== "#"
+        ) {
+          if (array[i + 1][j] !== "-") {
+            array[i + 1][j] = String(Number(array[i + 1][j]) + 1);
+          } else {
+            array[i + 1][j] = "1";
+          }
+        }
+        if (
+          i + 1 >= 0 &&
+          j + 1 >= 0 &&
+          i + 1 <= 4 &&
+          j + 1 <= 4 &&
+          array[i + 1][j + 1] !== "#"
+        ) {
+          if (array[i + 1][j + 1] !== "-") {
+            array[i + 1][j + 1] = String(Number(array[i + 1][j + 1]) + 1);
+          } else {
+            array[i + 1][j + 1] = "1";
+          }
+        }
+        // console.log("update array for the ", i, array);
+      }
+    }
+    //   }
+    //   copyOfArray = array;
+
+    //   console.log("copy", copyOfArray);
+    // console.log("original", array);
+  }
+
+  for (let i = 0; i < 5; i++) {
+    for (let j = 0; j < 5; j++) {
+      if (array[i][j] == "-") {
+        array[i][j] = "0";
+        // console.log("change innerElement: ", array[i][j]);
+      }
+    }
+  }
+  console.log(array);
+}
 
 // Examples
 // numGrid([
@@ -11,8 +149,8 @@
 //   ["-", "-", "-", "-", "-"],
 //   ["-", "-", "#", "-", "-"],
 //   ["-", "-", "-", "-", "-"],
-//   ["-", "-", "-", "-", "-"]
-// ]) ➞ [
+//   ["-", "-", "-", "-", "-"],
+// ]);➞ [
 //   ["0", "0", "0", "0", "0"],
 //   ["0", "1", "1", "1", "0"],
 //   ["0", "1", "#", "1", "0"],
@@ -25,8 +163,8 @@
 //   ["-", "-", "-", "-", "-"],
 //   ["-", "-", "#", "-", "-"],
 //   ["-", "-", "-", "-", "-"],
-//   ["#", "-", "-", "-", "-"]
-// ]) ➞ [
+//   ["#", "-", "-", "-", "-"],
+// ]);➞ [
 //   ["0", "0", "0", "1", "#"],
 //   ["0", "1", "1", "2", "1"],
 //   ["0", "1", "#", "1", "0"],
@@ -40,7 +178,8 @@
 //   ["-", "-", "#", "-", "-"],
 //   ["-", "#", "#", "-", "-"],
 //   ["-", "-", "-", "-", "-"]
-// ]) ➞ [
+// ])
+// ➞ [
 //   ["1", "1", "2", "#", "#"],
 //   ["1", "#", "3", "3", "2"],
 //   ["2", "4", "#", "2", "0"],
